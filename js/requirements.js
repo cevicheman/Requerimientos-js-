1dc9127ad79a5a0f08a6bd6ffc171e7d9b226968
Jack Vera <jackvera@espol.edu.ec>
2026-06-18 12:58:48 +0000

    creacion de la funcion flecha getSalesCoffee  que solo realiza un requerimiento asíncrono a coffe sales

--- js/requirements.js
+++ js/requirements.js
@@ -1,1 +1,10 @@
-let getSalesCoffee => 
+const getSalesCoffee = async () => {
+    const response = await fetch(
+        "https://raw.githubusercontent.com/DATA-DAWM/Datos/refs/heads/main/Coffee/Coffe_sales.xml"
+    );
+
+    const data = await response.text();
+
+    return data;
+};
+
